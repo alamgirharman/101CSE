83bfa0c4002df65d1cea608e208e5a7773dac6c7
alamgirharman <alamgirharman@gmail.com>
2020-12-25 01:17:04 +0600

    cpb

---
+++ beginningC/one3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Beware the Ides of March!");
+    return 0;
+}
---
+++ beginningC/one4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("My formula for success?\nRise early, work late, strike oil.\n");
+    return 0;
+}
